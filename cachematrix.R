c71f4faadecb102b6b4bb4f07a6b85071e3aaecb
mnossett <mnossett@yahoo.com>
2023-09-19 12:37:49 -0400

    code for programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates the cache environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+        x <<- y
+                }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of a matrix. It first looks through cache 
+## memory for the solution. If not found in cache memory, it will add it to
+## the cache memory for next time.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv) #returns inverse if it is in cache memory
+                }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)  #if it wasn't in cache before, solves and saves to cache
+        inv
 }
